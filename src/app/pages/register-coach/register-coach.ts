5fab0d3505f57b957d718e6174ed4975c3871082
bernardomoyano <bernardomofu@gmail.com>
2026-01-04 17:30:02 -0300

    commit

--- src/app/pages/register-coach/register-coach.ts
+++ src/app/pages/register-coach/register-coach.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { InputComponent as Input } from "../../components/input/input";
-import { Left } from "./left/left";
+import { Left } from "../register/left/left";
 import { Button } from "../../components/button/button";
 import { Logo } from "../../shared/components/logo/logo";
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-register-coach',
   imports: [RouterLink, Input, Left, Button, Logo],
-  templateUrl: './register.html',
-  styleUrl: './register.scss',
+  templateUrl: './register-coach.html',
+  styleUrl: './register-coach.scss',
 })
-export class Register {
+export class RegisterCoach {
 
 }
