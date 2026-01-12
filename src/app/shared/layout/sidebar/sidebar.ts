e1dd0ce95bd628bf1007a117558706a66ccb6582
bernardomoyano <bernardomofu@gmail.com>
2026-01-12 08:44:15 -0300

    create navbar

--- src/app/shared/layout/sidebar/sidebar.ts
+++ src/app/shared/layout/sidebar/sidebar.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Logo } from '../../components/logo/logo';
-import { StorageService } from '../../../services/storage.service';
 
 interface MenuItem {
   label: string;
@@ -12,42 +10,26 @@ interface MenuItem {
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, RouterLink, RouterLinkActive, Logo],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar implements OnInit {
-  userRole: string = '';
-  userName: string = 'Usuario';
   menuItems: MenuItem[] = [];
 
-  constructor(
-    private storageService: StorageService,
-    private router: Router
-  ) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.loadUserData();
     this.loadMenuItems();
   }
 
-  loadUserData(): void {
-    const userData = this.storageService.getUserData();
-    if (userData) {
-      this.userRole = userData.roleName || '';
-      this.userName = userData.name || 'Usuario';
-    }
-  }
-
   loadMenuItems(): void {
-    if (this.userRole === 'COACH') {
-      this.menuItems = [
-        { label: 'Planificaciones', icon: 'pi pi-calendar', route: '/planificaciones' },
-        { label: 'Atletas', icon: 'pi pi-users', route: '/atletas' },
-        { label: 'Ejercicios', icon: 'pi pi-list', route: '/ejercicios' },
-        { label: 'Mi Perfil', icon: 'pi pi-user', route: '/mi-perfil' }
-      ];
-    }
-    // Agregar más roles en el futuro (STUDENT, etc.)
+    // Por ahora solo para COACH, se puede expandir para otros roles
+    this.menuItems = [
+      { label: 'Planificaciones', icon: 'pi pi-calendar', route: '/planificaciones' },
+      { label: 'Atletas', icon: 'pi pi-users', route: '/atletas' },
+      { label: 'Ejercicios', icon: 'pi pi-list', route: '/ejercicios' },
+      { label: 'Mi Perfil', icon: 'pi pi-user', route: '/mi-perfil' }
+    ];
   }
 }
